c55b15847e40c4df0183fb6eb372710fa02f618c
Gergo Horanyi <gergo@pipetop.com>
2016-09-26 17:58:46 +0200

    Preparing model for async data download

--- dataimporter/models.py
+++ dataimporter/models.py
@@ -4,8 +4,17 @@ from django.db import models
 
 # Create your models here.
 class Document(models.Model):
+    PENDING = 1
+    READY = 2
+    DOWNLOAD_STATUS = (
+        (PENDING, 'Pending'),
+        (READY, 'Ready'),
+    )
+
     document_id = models.CharField(max_length=200)
-    creation_date = models.DateTimeField()
-    last_updated = models.DateTimeField()
-    document_type = models.CharField(max_length=100)
-    content = models.TextField()
+    creation_date = models.DateTimeField(blank=True, null=True)
+    last_updated = models.DateTimeField(blank=True, null=True)
+    document_type = models.CharField(max_length=100, blank=True, null=True)
+    content = models.TextField(blank=True, null=True)
+    size = models.IntegerField(blank=True, null=True)
+    download_status = models.IntegerField(choices=DOWNLOAD_STATUS, default=PENDING)
