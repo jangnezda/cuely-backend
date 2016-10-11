90e3c1cb68b9aa25d28abb949a2071098030e530
Jan Gnezda <jan@pipetop.com>
2016-10-11 09:38:13 +0200

    separate celery beat into another container

---
+++ start_beat.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+
+# start celery 'beat' - a scheduler for periodic tasks
+
+
+rm -rf /etc/celery_beat*.pid
+rm -rf /var/log/celery_beat*.log
+
+cd /usr/src/app
+
+celery -A cuely --detach --logfile=/var/log/celery_beat.log --pidfile=/etc/celery_beat.pid beat --loglevel=info
+sleep 5
+tail -F /var/log/celery_beat.log
