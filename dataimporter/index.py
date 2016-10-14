65c484058cf057e1621a150b8c19e1cebe327bc6
Jan Gnezda <jan@pipetop.com>
2016-10-14 10:52:31 +0200

    Apply better settings to algolia indexing

--- dataimporter/index.py
+++ dataimporter/index.py
@@ -1,5 +1,43 @@
 from django.contrib.algoliasearch import AlgoliaIndex
 import os
 
+
 class DocumentIndex(AlgoliaIndex):
     index_name = os.environ["ALGOLIA_INDEX_NAME"]
+    settings = {
+        # list of attributes that are used for searching
+        'attributesToIndex': [
+            'unordered(title)',
+            'owner_displayName',
+            'lastModifyingUser_displayName',
+            'content'
+        ],
+        # adjust ranking formula
+        'ranking': [
+            'typo',
+            'filters',
+            'proximity',
+            'exact',
+            'attribute',
+            'desc(last_updated_ts)',
+            'words'
+        ]
+    }
+    # only following Document fields will be synced to Algolia
+    fields = (
+        'document_id',
+        'title',
+        'last_updated_ts',
+        'last_updated',
+        'content',
+        'mimeType',
+        'webViewLink',
+        'requester',
+        'user_id',
+        'iconLink',
+        'owner_displayName',
+        'owner_photoLink',
+        'lastModifyingUser_displayName',
+        'lastModifyingUser_photoLink',
+        'thumbnailLink'
+    )
