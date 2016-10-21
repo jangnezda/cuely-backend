2e6e256441c9cc30a4bca80fed013979780a2c54
Jan Gnezda <jan@pipetop.com>
2016-10-21 15:25:04 +0200

    Add email to json response for frontend

--- frontend/views.py
+++ frontend/views.py
@@ -1,21 +1,17 @@
 from django.http import JsonResponse
 from django.shortcuts import render, redirect
-from django.template import RequestContext
-from django.contrib.auth import authenticate
 from django.http import HttpResponseForbidden
 
 from dataimporter.models import Document
-from dataimporter.tasks import collect_gdrive_docs, start_synchronization as docs_sync
+from dataimporter.tasks import start_synchronization as docs_sync
+
 
 def index(request):
     documents = []
     backends = []
     if request.user.is_authenticated:
         try:
-            social = request.user.social_auth.get(provider='google-oauth2')
             backends.append("google-oauth2")
-            access_token = social.extra_data['access_token']
-            refresh_token = social.extra_data['refresh_token']
             documents = Document.objects.filter(requester=request.user)
         except:
             pass
@@ -25,7 +21,7 @@ def index(request):
 def start_synchronization(request):
     try:
         if request.user.is_authenticated:
-            docs_sync(user=request.user) 
+            docs_sync(user=request.user)
     except:
         pass
     return redirect('/home/')
@@ -35,7 +31,7 @@ def sync_status(request):
     user = request.user
     if user.is_authenticated:
         documents_count = Document.objects.filter(requester=user).count()
-        documents_ready_count = Document.objects.filter(requester=user).filter(download_status = Document.READY).count()
+        documents_ready_count = Document.objects.filter(requester=user).filter(download_status=Document.READY).count()
         return JsonResponse({
             "documents": documents_count,
             "ready": documents_ready_count,
@@ -50,6 +46,7 @@ def get_algolia_key(request):
         return JsonResponse({
             'userid': request.user.id,
             'username': request.user.username,
+            'email': request.user.email,
             'appId': 'OPDWYH4IR4',
             'searchKey': '0b28a5913167a1618773992171c04344'
         })
